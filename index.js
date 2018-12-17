e39e4f303b2ce005726a065d89958acdff04e534
peaceful-trainer-9354 <vladigolnikov@gmail.com>
2018-12-17 02:42:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
-  var instruments = ['']
+  var instruments = ['guitar', '']
 }
